e698dcd1b0d83e1a211da56b47e85f5c428b12ac
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-24 08:50:56 -0700

    add venus first test

---
+++ __tests__/venus.test.js
@@ -0,0 +1,8 @@
+import venus from "./../src/js/venus.js";
+
+describe("Venus", () => {
+  test("should correctly create ratio of earth to venus", () => {
+    const spaceTime = new Venus(.62);
+    expect(spaceTime.earthtime).toEqual(.62);
+  });
+});
