1fdfa4a50f35416040a996154da7f3d0f167ab74
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-24 09:06:34 -0700

    add passing second test for mars

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -5,5 +5,8 @@ describe("Mars", () => {
     const spaceTime = new Mars(1.88);
     expect(spaceTime.earthtime).toEqual(1.88);
   });
-
+  test("Should output the the calculation to marstime", () => {
+    const spaceTime = new Mars(1);
+    expect(spaceTime.marsTime(1)).toEqual(1/1.88);
+  });
 });
--- src/js/mars.js
+++ src/js/mars.js
@@ -2,4 +2,7 @@ export default class Mars {
   constructor (earthtime){
     this.earthtime = earthtime;
   }
+  marsTime(earthtime){
+    return earthtime/1.88;
+  }
 }
