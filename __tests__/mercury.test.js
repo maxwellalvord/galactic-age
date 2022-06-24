a6d34bf2fc5a1c025518edffebf1122b88c36ebd
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-24 08:33:04 -0700

    Add Mercury class and have first test pass

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -3,6 +3,6 @@ import Mercury from "./../src/js/mercury.js";
 describe("Mercury", () => {
   test("should correctly create ratio of earth to mercury", () => {
     const spaceTime = new Mercury(.24);
-    expect(spaceTime).toEqual(.24);
+    expect(spaceTime.ratio).toEqual(.24);
   });
 });
---
+++ src/js/mercury.js
@@ -0,0 +1,5 @@
+export default class Mercury {
+  constructor (ratio){
+    this.ratio = ratio;
+  }
+}
